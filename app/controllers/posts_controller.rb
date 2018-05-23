b9fae09e723c19d15533ccd960218d6c6a79c990
Jared Murphy <jaredcmurphy@gmail.com>
2018-05-23 02:13:36 -0400

    adds functionality for creating new posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController 
+  def new 
+    @post = Post.new(user: current_user)
+  end
+
+  def create 
+    post = Post.new(post_params)
+    
+    if post.save
+      flash[:notice] = "Post successfully created"
+      redirect_to root_path
+    else 
+      flash[:alert] = "Oops something went wrong"
+      redirect_to root_path
+    end
+  end
+
+  private 
+
+  def post_params 
+    params.require(:post).permit(:user_id, :title, :body)
+  end
+end
