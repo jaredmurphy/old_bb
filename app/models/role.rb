4dba78dd17d491b7462b467cad403d4ac991ad89
Jared Murphy <jaredcmurphy@gmail.com>
2018-05-23 00:54:57 -0400

    adds getter methods for user roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -6,4 +6,16 @@ class Role < ApplicationRecord
   def self.default
     Role.find_by(name: "user")
   end
+
+  def self.publisher
+    Role.find_by(name: "publisher")
+  end
+
+  def self.moderator
+    Role.find_by(name: "moderator")
+  end
+
+  def self.admin
+    Role.find_by(name: "admin")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+    :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_many :user_roles
-
   after_create :assign_default_values
+
+  def publisher?
+    self.user_roles.include? Role.publisher
+  end
+
+  def moderator?
+    self.user_roles.include? Role.moderator
+  end
+
+  def admin?
+    self.user_roles.include? Role.admin
+  end
 
   private 
 
