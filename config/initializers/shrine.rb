13ca4d2d479417afc52e0a1646c25700b1f36bf0
jaredmurphy <jaredcmurphy@gmail.com>
2019-07-02 23:20:14 -0400

    use secrets instead of accessing ENV directly

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -3,16 +3,24 @@ require "shrine/storage/file_system"
 require "shrine/storage/s3"
 
 s3_options = {
-  access_key_id: ENV["SPACES_KEY"],
-  secret_access_key: ENV["SPACES_SECRET"],
-  bucket: ENV["SPACES_BUCKET"],
-  endpoint: ENV["SPACES_ENDPOINT"],
-  region: ENV["SPACES_REGION"]
+  access_key_id: Rails.application.secrets.spaces_key,
+  secret_access_key: Rails.application.secrets.spaces_secret,
+  bucket: Rails.application.secrets.spaces_bucket,
+  endpoint: Rails.application.secrets.spaces_endpoint,
+  region: Rails.application.secrets.spaces_region
 }
 
 Shrine.storages = {
-  cache: Shrine::Storage::S3.new(prefix: ENV["SPACES_CACHE_PREFIX"], upload_options: {acl: "public-read"}, **s3_options),
-  store: Shrine::Storage::S3.new(prefix: ENV["SPACES_STORE_PREFIX"], upload_options: {acl: "public-read"}, **s3_options)
+  cache: Shrine::Storage::S3.new(
+    prefix: Rails.application.secrets.spaces_cache_prefix,
+    upload_options: {acl: "public-read"},
+    **s3_options
+  ),
+  store: Shrine::Storage::S3.new(
+    prefix: Rails.application.secrets.spaces_store_prefix,
+    upload_options: {acl: "public-read"},
+    **s3_options
+  )
 }
 
 Shrine.plugin :activerecord
