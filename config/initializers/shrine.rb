4d6227e1eca773fd088c2dfd63c4243ed67a5326
jaredmurphy <jaredcmurphy@gmail.com>
2019-07-02 22:46:12 -0400

    add spaces integration

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,9 +1,18 @@
 require "shrine"
 require "shrine/storage/file_system"
+require "shrine/storage/s3"
+
+s3_options = {
+  access_key_id: ENV["SPACES_KEY"],
+  secret_access_key: ENV["SPACES_SECRET"],
+  bucket: ENV["SPACES_BUCKET"],
+  endpoint: ENV["SPACES_ENDPOINT"],
+  region: ENV["SPACES_REGION"]
+}
 
 Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"), # permanent
+  cache: Shrine::Storage::S3.new(prefix: ENV["SPACES_CACHE_PREFIX"], upload_options: {acl: "public-read"}, **s3_options),
+  store: Shrine::Storage::S3.new(prefix: ENV["SPACES_STORE_PREFIX"], upload_options: {acl: "public-read"}, **s3_options)
 }
 
 Shrine.plugin :activerecord
