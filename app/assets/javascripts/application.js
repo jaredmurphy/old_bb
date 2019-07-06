a3bd1e074a80d0d48d0a820aa53795aac09a47d5
jaredmurphy <jaredcmurphy@gmail.com>
2019-07-05 20:36:09 -0400

    add basic navigation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,44 @@
 //= require turbolinks
 //= require trix
 //= require_tree .
+
+  // Show an element
+const show = (elem) => {
+  elem.style.display = 'block';
+};
+
+// Hide an element
+const hide = (elem) => {
+  elem.style.display = 'none';
+};
+
+// Toggle element visibility
+const toggle = (elem) => {
+
+  // If the element is visible, hide it
+  if (window.getComputedStyle(elem).display === 'block') {
+    hide(elem);
+    return;
+  }
+
+  // Otherwise, show it
+  show(elem);
+}
+
+// hambuger menu click
+document.addEventListener('DOMContentLoaded', function(){
+  document.getElementById('hamburger').addEventListener('click', (event) => {
+    event.preventDefault();
+    toggle(document.getElementById('nav_mobile'));
+    toggle(document.getElementById('container'));
+  });
+});
+
+window.addEventListener('resize', function () {
+  const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+  if (viewportWidth > 768) {
+    hide(document.getElementById('nav_mobile'));
+    show(document.getElementById('container'));
+  }
+});
+
